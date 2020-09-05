4d7a47a1a04381cc9e3966aad5692a27accb7430
user <user@usernoMacBook-Air.local>
2020-09-05 12:27:18 +0900

    Add users_controller_with_show_templete & Delete devise:views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  root 'comments#index'
-  get 'comments/index'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/show'
+  devise_scope :user do
+    root 'devise/sessions#new'
+  end
+
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
+
+  resources :users, only: [:index, :show]
+
 end
