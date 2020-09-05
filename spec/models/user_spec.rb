2160260b86fcd169f41f6c644905ff90fd596a8b
user <user@usernoair.lan>
2020-09-05 20:36:04 +0900

    Add Initial_test_rspec for user_models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "isn't less than 1 character" do
-    user = User.new
-    expect(user).not_to be_valid
+  it "is valid with name, email, password" do
+    user = User.new(name:     "bob",
+                    email:    "bob@example.com",
+                    password: "password"
+                    )
+    expect(user).to be_valid
   end
 end
