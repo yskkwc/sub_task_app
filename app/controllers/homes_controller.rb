dd497ab2ad83386fa740dc02492aebf5f6c1b4f5
user <user@usernoair.lan>
2020-09-06 23:18:15 +0900

    Add homes_controller

---
+++ app/controllers/homes_controller.rb
@@ -0,0 +1,6 @@
+class HomesController < ApplicationController
+  def policy
+  end
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show]
 
+  get '/policy'  => 'homes#policy'
+  get '/contact' => 'homes#contact'
 end
