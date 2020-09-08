c1d67ecc7774af168504a6903fce4f2db57aa7b6
user <user@usernoMacBook-Air.local>
2020-09-08 19:53:28 +0900

    Gems_jquery-rails/uplifter & Change Users_controller/show_templete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!
 
   def index
   end
 
   def show
+    @user = current_user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
 
-  resources :users, only: [:index, :show]
+  resources :users
 
   get '/policy'  => 'homes#policy'
   get '/contact' => 'homes#contact'
