dc498dd0df000d617c2a56cfab499fd12ed84c34
user <user@usernoair.lan>
2020-09-10 21:33:11 +0900

    Add new_tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,18 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def log_in( user )
+    if integration_test?
+      #use warden helper
+      login_as(user, :scope => :user)
+    else #controller_test, model_test
+      #use devise helper
+      sign_in(user)
+    end
+  end
+
+  # 統合テスト内ではtrueを返す
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
