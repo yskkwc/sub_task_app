845496c429a4742cc952b63f52e8dcbf9a8ebf16
user <user@usernoMacBook-Air.local>
2020-09-12 15:14:25 +0900

    Change registrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
                   length: { maximum: 50 }
 
       def self.from_omniauth(auth)
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+            where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
             user.email = auth.info.email
             user.password = Devise.friendly_token[0,20]
             user.name = auth.info.name   # assuming the user model has a name
@@ -17,5 +17,18 @@ class User < ApplicationRecord
             # uncomment the line below to skip the confirmation emails.
             # user.skip_confirmation!
             end
+      end
+
+      def update_without_current_password(params, *options)
+            params.delete(:current_password)
+
+            if params[:password].blank? && params[:password_confirmation].blank?
+                  params.delete(:password)
+                  params.delete(:password_confirmation)
+            end
+
+            result = update_attributes(params, *options)
+            clean_up_passwords
+            result
       end
 end
