1322d2a0e5157e8146a463e749297bca3e0e3515
user <user@usernoair.lan>
2020-09-21 09:35:28 +0900

    Add Micropost_tests_contain_errors_yet

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -18,4 +18,12 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to new_user_session_url
   end
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
+  end
 end
