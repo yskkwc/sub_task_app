e3f1da7a12359914eef56de1dec6d0812b953e79
user <user@usernoair.lan>
2020-09-21 12:50:46 +0900

    Add Micropost_delete_action

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,6 @@
 class MicropostsController < ApplicationController
+  before_action :correct_user,   only: :destroy
+
   def new
     @micropost = current_user.microposts.build if user_signed_in?
   end
@@ -17,10 +19,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    flash[:success] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
 
   private
     def micropost_params
       params.require(:micropost).permit(:content, :image)
+    end
+
+    def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,5 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.assets.js_compressor = :uglifier
+  config.active_storage.service = :amazo
 end
