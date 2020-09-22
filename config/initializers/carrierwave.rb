3d2c3c3ce77b83e52cbe9c6645632aefc7a56742
user <user@usernoair.lan>
2020-09-23 01:05:52 +0900

    Change acess_key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'sub-task-carrierwave-strage' # バケット名
+    config.fog_directory  = 'sub-task-app-strage' # バケット名
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
