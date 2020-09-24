e405802a2b8b370e7b5ef60081931fd285ee3944
user <user@usernoMacBook-Air.local>
2020-09-24 15:55:36 +0900

    Add following/followed_views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,14 +30,14 @@ class UsersController < ApplicationController
     @title = "Following"
     @user  = User.find(params[:id])
     @users = @user.following.paginate(page: params[:page])
-    render 'show_follow'
+    render 'show_following'
   end
 
   def followers
     @title = "Followers"
     @user  = User.find(params[:id])
     @users = @user.followers.paginate(page: params[:page])
-    render 'show_follow'
+    render 'show_follower'
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ User.create!(name:                   "管理者",
 
 30.times do |n|
   name     = Faker::Name.name
-  username = Faker::Name.name
+  username = name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
   User.create!(name:                   name,
@@ -20,10 +20,13 @@ end
 
 #userのmicropost投稿
 users = User.order(:created_at).take(6)
+
 40.times do
   content = Faker::Lorem.sentence(word_count: 5)
-  users.each { |user| user.microposts.create!(content: content) }
+  users.each { |user| user.microposts.create!(content: content)}
 end
+
+
 
 #relationships
 users = User.all
