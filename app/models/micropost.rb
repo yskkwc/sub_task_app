7379b25ee5c0c4b6ab4357551b02ec2c6ca5a286
user <user@usernoair.lan>
2020-09-25 13:09:04 +0900

    Change users/microposts_models_has_many_comments

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :comments
   has_one_attached :post_image
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
