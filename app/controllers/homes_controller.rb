1aca7ff701dba730df9f9c9ce1427919a2aa586c
yskkwc <y.k.trial.serv@gmail.com>
2020-10-06 21:37:21 +0900

    Change Homes_controller & carrierwave.rb_for_honban

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,8 @@
 class HomesController < ApplicationController
   def home
+    if user_signed_in?
+      redirect_to users_path
+    end
   end
 
   def policy
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,12 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'sub-task-carrierwave' # バケット名
+    config.fog_directory  = 'sub-task-app-strage' # 作成したバケット名を記述
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      AWS_REGION: 'ap-northeast-1',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], # 環境変数
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'], # 環境変数
+      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
       path_style: true
     }
 end
